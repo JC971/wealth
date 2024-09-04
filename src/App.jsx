1c40c852817e46d057355fd668fa8e00580900a8
jean-claude <jean-claude.grech@orange.fr>
2024-09-04 09:32:37 +0200

    index chemin

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 	return (
 		<div>
 			<h1>Welcome to My React App</h1>
-			<p>This is a simple React application.</p>
+	
 		</div>
 	);
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,20 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const { Children } = require('react');
+const { Stats } = require('webpack');
 
 module.exports = {
-  mode: 'development',  
-  entry: './src/index.jsx',  
+  mode: 'development',
+  entry: './src/index.jsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+ 
   },
   module: {
     rules: [
       {
-        test: /\.jsx?$/,  
+        test: /\.jsx?$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
@@ -22,11 +26,16 @@ module.exports = {
     ],
   },
   resolve: {
-    extensions: ['.js', '.jsx'], 
+    extensions: ['.js', '.jsx'],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html', 
+    }),
+  ],
   devServer: {
     static: path.resolve(__dirname, 'dist'),
     compress: true,
-    port: 8840,
+    port: 22736,
   },
 };
